f7ce6273bbbb71d3d253683390a4992d90fa2d80
zsadr67 <sadrzahra88@gmail.com>
2025-09-06 13:07:28 +0330

    add user/serializer in user app

---
+++ user/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.authtoken.admin import User
+from rest_framework.serializers import ModelSerializer
+
+
+class CreateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username' ,'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
