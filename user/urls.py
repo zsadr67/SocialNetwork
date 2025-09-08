ab63e5bbfb935beb27804e2a5b695806906029c1
zsadr67 <sadrzahra88@gmail.com>
2025-09-08 16:37:17 +0330

    space

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
