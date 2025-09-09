5aeed03d5c4041a55659efe971b15c40d9725ffa
zsadr67 <sadrzahra88@gmail.com>
2025-09-09 12:30:09 +0330

    add model & serializer & view section Post and edit serializer & view section Profile for permission

--- social/models.py
+++ social/models.py
@@ -13,3 +13,12 @@ class Profile(models.Model):
 
 
 
+class Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
--- social/views.py
+++ social/views.py
@@ -1,9 +1,43 @@
-from rest_framework import viewsets
 
-from social.models import Profile
-from social.serializers import ProfileSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import permissions
+
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from social.models import Profile, Post
+from social.serializers import ProfileSerializer, PostSerializer
+
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
 
 
 class ProfileView(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+    authentication_classes = [JWTAuthentication]
+
+    queryset = Profile.objects.select_related("user").all()
     serializer_class = ProfileSerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+
+class PostView(viewsets.ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def perform_create(self, serializer):
+    #     # موقع ساخت پست، نویسنده همون یوزر لاگین شده باشه
+    #     serializer.save(user=self.request.user)
+
